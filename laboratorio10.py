e744db3b439076a70b380361757dcd0cfaa05845
Henryal12 <henry.rueda.2020@upb.edu.co>
2020-04-11 17:17:47 -0500

    Modificacion con multiples a de entrada

--- laboratorio10.py
+++ laboratorio10.py
@@ -9,11 +9,21 @@ potencia=0
 a=int(input("ingrese a: "))
 b=int(input("ingrese b: "))
 suma=1
-for i in range(b):
-    potencia= a*suma
-    suma=potencia
-
+if a!=0:
+    if b!=0:
+        for i in range(b):
+            potencia= a*suma
+            suma=potencia
+    else:
+        b=1
+        for i in range(b):
+            potencia= a/a
+else:
+    potencia=0
 def a_power_b():
-    print("el resultado de la potencia es: " + str(potencia))
+    if a!=0:
+        print("el resultado de la potencia es: " + str(int(potencia)))
+    else:
+        print("el programa se ha detenido")
     
 a_power_b()
